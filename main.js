23798abf94b0a79d21ea18b9a6d0ca63cb130d52
kamaleshwaran2002 <ks110302@gmail.com>
2023-04-12 13:04:02 +0530

    add ing function

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const body = document.body
 const btn = document.querySelector('#btn')
 const firstInput = document.querySelector('#input1')
 const secondInput = document.querySelector('#input2')
@@ -5,7 +6,34 @@ const secondInput = document.querySelector('#input2')
 
 btn.addEventListener('click', func);
 
-function func(e){
-    console.log(e)
-    console.log(firstInput.value)
+const succes = "successfull"
+
+function func(e) {
+
+    const text = document.createElement('p')
+    // if(((firstInput.value == 1) < (secondInput.value == 2)) || ((firstInput.value==2) < (secondInput.value==3)) || ((firstInput.value==3) < (secondInput.value==1))){
+    //     console.log("successfull-1");
+    //     text.innerText = "successfull-1"
+    //     body.append(text)
+
+    // }
+
+
+    if ((firstInput.value) < (secondInput.value)) {
+        console.log("successfull-1");
+        text.innerText = "successfull-1"
+        body.append(text)
+    }
+    // else if ((firstInput.value==3) > (secondInput.value==3)){
+    //     console.log("successfull-2");
+    //     text.innerText = "successfull-2"
+    //     body.append(text)
+    // }
+    else {
+        console.log("failed")
+        text.innerText = "failed"
+        body.append(text)
+
+    }
+
 }
